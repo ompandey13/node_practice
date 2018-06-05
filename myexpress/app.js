cd31e2bd8a4834e380857be51d78f3bfa3c1c54a
ompandey13 <om.pandey@crownstack.com>
2018-06-06 02:36:59 +0530

    Feature: pug template engine

--- myexpress/app.js
+++ myexpress/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var pug = require('pug');
 
 var port = 3000;
 
@@ -12,12 +13,18 @@ app.use(function(req, res, next) {
 	next();
 });
 
+app.set('view engine', 'pug');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
-	res.send('Hello world!');
+	res.render('index', {
+		title: 'Hello World',
+		showTitle: true,
+		people: ['john', 'stvee', 'mark']
+	});
 });
 app.get('/about', function(req, res) {
 	res.send('About page!');
